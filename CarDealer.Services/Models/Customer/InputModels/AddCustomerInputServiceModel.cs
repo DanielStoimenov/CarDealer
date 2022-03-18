4d7a1435515981d9d919cd902c346b6c69bcd655
Daniel Stoimenov <daniel.stoimenov@itsoft.bg>
2022-03-18 14:09:07 +0200

    Add new Controllers/CustomersController.cs

--- CarDealer.Services/Models/Customer/InputModels/AddCustomerInputServiceModel.cs
+++ CarDealer.Services/Models/Customer/InputModels/AddCustomerInputServiceModel.cs
@@ -9,6 +9,7 @@ namespace CarDealer.Services.Models.Customer.InputModels
         [MaxLength(30)]
         public string Name { get; set; }
 
+        [Required]
         public DateTime BirthDate { get; set; }
 
         public bool IsYoungDriver { get; set; }
---
+++ CarDealer.Web/Controllers/CustomersController.cs
@@ -0,0 +1,21 @@
+﻿using CarDealer.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarDealer.Web.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomersController(ICustomerService customerService)
+        {
+            this._customerService = customerService;
+        }
+
+        public IActionResult Index()
+        {
+            var customers = _customerService.GetAll();
+            return View(customers);
+        }
+    }
+}
